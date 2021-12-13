8f58f8cb1cdcd859914d477a268119dc8e17ddca
Skkay <skkay.thomas@gmail.com>
2021-12-13 16:41:33 +0100

    Add course signing for next courses

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ else:
 
 API_GET_THREE_MONTHS_COURSES = config['API_GET_THREE_MONTHS_COURSES']
 API_POST_SET_STUDENT_PRESENT = config['API_POST_SET_STUDENT_PRESENT']
+API_GET_NEXT_USER_COURSES = config['API_GET_NEXT_USER_COURSES']
 JWT = config['JWT']
 SCHOOL_ID = config['SCHOOL_ID']
 STUDENT_ID = config['STUDENT_ID']
@@ -19,18 +20,35 @@ BASE64_SIGNATURE = config['BASE64_SIGNATURE']
 
 def main():
     last_courses = get_last_three_months_courses()
+    next_courses = get_next_user_courses()
+
     for course in last_courses['result']:
         if course['STUDENT_PRESENCE'] == False:
             success = post_set_student_present(course['ID'])
 
             if success:
-                print('Successfully signed for course {course_name} ({course_id})'.format(course_name=course['NAME'], course_id=course['ID']))
+                print('Successfully signed for course {course_name} ({course_id}) (from last_courses)'.format(course_name=course['NAME'], course_id=course['ID']))
 
+    for course in next_courses['result']['data']:
+        if course['isWaitingSign'] == True:
+            success = post_set_student_present(course['ID'])
+
+            if success:
+                print('Successfully signed for course {course_name} ({course_id}) (from next_courses)'.format(course_name=course['NAME'], course_id=course['ID']))
 
 def get_last_three_months_courses():
     res = requests.get(API_GET_THREE_MONTHS_COURSES, headers={'authorization': 'Bearer ' + JWT})
 
     if (res.status_code == 200):
+        return res.json()
+    
+    print(f'Error: {res.status_code=}, {res.text=}')
+    exit(1)
+
+def get_next_user_courses():
+    res = requests.get(API_GET_NEXT_USER_COURSES, headers={'authorization': 'Bearer ' + JWT})
+
+    if res.status_code == 200:
         return res.json()
     
     print(f'Error: {res.status_code=}, {res.text=}')
